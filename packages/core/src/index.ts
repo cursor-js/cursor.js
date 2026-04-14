43be629b6cfcd3e3fbfae31147d4599672b4d033
Cihad Paksoy <cihadp@gmail.com>
2026-04-14 19:08:10 +0300

    refactor: move demo code to docs and clean up core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
-export { Cursor, type CursorOptions } from "./Cursor";
-export { GhostCursor } from "./core/GhostCursor";
-export { EventDispatcher } from "./core/EventDispatcher";
-export * from "./core/utils";
+export { Cursor, type CursorOptions } from './Cursor';
+export { GhostCursor } from './core/GhostCursor';
+export { EventDispatcher } from './core/EventDispatcher';
+export * from './core/utils';
