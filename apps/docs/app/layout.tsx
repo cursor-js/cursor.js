9e932d8566c0fddbf2a60e65fd977aee968b28c3
Cihad Paksoy <cihadp@gmail.com>
2026-04-14 21:33:16 +0300

    chore(docs): upgrade fumadocs and next.js to v16 with RootProvider migration

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,9 +1,9 @@
-import { RootProvider } from 'fumadocs-ui/provider';
+import { RootProvider } from 'fumadocs-ui/provider/next';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
